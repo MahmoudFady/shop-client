6c470532f3498345914e5f42bbabf8a7c9049a29
mahmoud fady <mahmoudfady02@gmail.com>
2023-06-17 01:31:58 +0300

    fix cart base url

--- src/app/core/services/cart.service.ts
+++ src/app/core/services/cart.service.ts
@@ -1,11 +1,12 @@
-import { Subject } from 'rxjs';
+import { BehaviorSubject, Subject } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ICart } from '../shared/models/cart.model';
+import { environment } from 'src/environments/environment.development';
 @Injectable({ providedIn: 'root' })
 export class CartService {
-  private readonly baseUrl = 'http://localhost:3000/api/cart/';
-  private cart$ = new Subject<ICart>();
+  private readonly baseUrl = environment.baseUrl + 'cart/';
+  private cart$ = new BehaviorSubject<ICart | null>(null);
   constructor(private http: HttpClient) {}
   private cartObserver = {
     next: (response: { cart: ICart }) => {
